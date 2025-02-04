cce76efa431eab5822da45408c4a082dd09afd43
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-02-04 21:10:14 +0530

    add socket video-call folder

---
+++ socket-videocall/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='text-2xl text-center'>
+      hello
+     </div>
+    </>
+  )
+}
+
+export default App
