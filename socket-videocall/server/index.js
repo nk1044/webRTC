6c31c0ef1524006fce3c9e37d33ef18bc8d1f395
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-02-12 18:51:11 +0530

    add health-check route to server for monitoring

--- socket-videocall/server/index.js
+++ socket-videocall/server/index.js
@@ -5,6 +5,16 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// Define a simple route to check the server
+app.get('/health-check', (req, res) => {
+    try {
+        const response = fetch('https://wakeup-server:01.onrender.com/health-check');
+    } catch (error) {
+        console.log('Error in health-check of wakup server', error);
+    }
+    res.send('Hello World');
+});
+
 // Start the server listening on port 8000
 server.listen(8000, () => {
     console.log('Server is running on port 8000');
