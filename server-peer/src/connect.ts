1284b165f03df48440544de3618e9d293f9c2a7e
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-04-17 06:04:08 +0530

    feat: add video call event handling for offer, answer, and ice-candidate

--- server-peer/src/connect.ts
+++ server-peer/src/connect.ts
@@ -21,6 +21,21 @@ class ConnectPeer {
       socket.on('disconnect', () => {
         console.log('user disconnected, inside library');
       });
+
+      // video call starts
+      socket.on("offer", ({ roomId, offer }) => {
+        socket.to(roomId).emit("offer", { offer });
+      });
+
+      socket.on("answer", ({ roomId, answer }) => {
+        socket.to(roomId).emit("answer", { answer });
+      }); 
+
+      socket.on("ice-candidate", ({ roomId, candidate }) => {
+        socket.to(roomId).emit("ice-candidate", { candidate });
+      });
+      // video call ends
+
     });
   }
 }
